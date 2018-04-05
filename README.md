d59d1f7aa4d1203a6720820f7a6157d910482b78
Andriy Klitsuk <andriy.klitsuk@risolviamo.com>
2018-04-05 13:27:39 +0200

    readme edit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # inverted-text
 Set custom inverted background texture to text on images
 
@@ -5,8 +6,97 @@ Set custom inverted background texture to text on images
 ![](https://media.giphy.com/media/BppJCuTwSSJ6RR9XtK/giphy.gif)
 ![](https://media.giphy.com/media/25QTZ9dH8vXwJvRM8g/giphy.gif)
 
+## Install
+```
+npm i inverted-text
+```
 
-<span style="color:red">### DRAFT</span>
+## Configuration
+Use the HTML code below to display a image.  Set the appropriate dimensions to the div.
+```html
+<div id="img-wrapper" style="background-image: url('image.jpg'); width: 100%; height: 400px">
+      <p>Text</p>
+      ...
+</div>
+```
+then use the following javascript code  to do your magic:
 ```javascript
-new InvertedText(options, [callback], [callback])
+let InvertedText = require('inverted-text');
+new InvertedText();
+```
+It automatically search for a `div` with `id="img-wrapper`. Use the options to set a different id.
+The invert texture effect is applied to all the HTML text nodes tags inside the image wrapper div, so nothing is done if the text is not inside a tag.
+
+*eg*
+```javascript
+new InvertedText({
+  image_wrapper_id: "wrp", greyscale: 200, contrast: 1000, saturate: 100, invert: 100},
+  (invt) => {
+  let text = document.getElementById('text');
+
+  setTimeout( () => {
+    text.style.right = '800px';
+
+    let updateInterval = setInterval(() => {
+      invt.update(null, () => console.log("updated"));
+    }, 10);
+
+    setTimeout( () => {
+      clearInterval(updateInterval);
+      invt.update(null, () => console.log("updated"));
+    }, 1000);
+  }, 1000);
+
+ },
+ (invt) => console.log("resize", invt)
+);
+```
+<br>
+
+ The `position` property of every text node is setted to `absolute`, while the `position` property of the wrapper div is setted to `relative`. The text nodes can be positioned inside the wrapper by `top`, `left`, `bottom`, `right` properties
+
+<br>
+
+
+
+## Details
+#### Create a new Object
+```javascript
+new InvertedText(options, [onLoaded], [everyResize]);
+```
+**options** is a obj with the following parameters:
+ - **image_wrapper_id** *String* - id of the image wrapper div.
+ - **greyscale** *Number* - value of CSS filter  property greyscale
+ - **contrast** *Number* - value of CSS filter  property contrast
+ - **saturate** *Number*  - value of CSS filter  property saturate
+ - **invert** *Number* - value of CSS filter  property invert
+
+```javascript
+{
+  image_wrapper_id: "wrp",
+  greyscale: 200,
+  contrast: 1000,
+  saturate: 100,
+  invert: 100
+}
+```
+**onLoaded** *function* - called when the rendering process is finished
+**everyResize** *function* - called when the screen is resized
+
+<br><br>
+#### Update a rendered text
+
+```javascript
+function update([eachCallback], [callback])
+```
+<br><br>
+#### Check for options and render the text. It is called by the constructor
+
+```javascript
+function bidibibodibibu(options, [callback])
+```
+<br><br>
+#### Get all text nodes in the node
+```javascript
+function getAllTextNodes(node)
 ```
--- src/inverted-text.js
+++ src/inverted-text.js
@@ -3,13 +3,19 @@
 class InvertedText {
 
   constructor(options, callback, everyResize) {
+    this.options = options;
     this.callback = callback;
     this.everyResize = everyResize;
-    this.image_wrapper_id = options.image_wrapper_id || 'img-wrapper';
-    this.invert = options.invert || 100;
-    this.saturate = options.saturate || 100;
-    this.contrast = options.contrast || 1000;
-    this.grayscale = options.grayscale || 200;
+
+    if(options != undefined) {
+      this.image_wrapper_id = options.image_wrapper_id || 'img-wrapper';
+      this.invert = options.invert || 100;
+      this.saturate = options.saturate || 100;
+      this.contrast = options.contrast || 1000;
+      this.grayscale = options.grayscale || 200;
+    }
+
+    console.log(this-image_wrapper_id);
 
     window.addEventListener("load", () => {
       this.bidibibodibibu(options, this.callback);
@@ -20,41 +26,68 @@ class InvertedText {
       this.everyResize();
     }, false);
 
+    return this;
+
   }
 
   bidibibodibibu(options, fn) {
-    let wrapper = document.getElementById(this.image_wrapper_id);
-    let textNodes = this.getAllTextNodes(wrapper);
-    let img_src_url = wrapper.style.backgroundImage;
+    console.log("render");
+    this.wrapper = document.getElementById(this.image_wrapper_id);
+    this.textNodes = this.getAllTextNodes(this.wrapper);
+
+    let img_src_url = this.wrapper.style.backgroundImage;
 
     img_src_url = img_src_url.substring(img_src_url.indexOf('"') + 1, img_src_url.length);
     img_src_url = img_src_url.substring(0, img_src_url.indexOf('"'));
 
-    let wrapper_bg_data = this.wrapper.style.backgroundPosition;
-    let wrapper_bg_x = parseInt(wrapper_bg_data.split(" ")[0]);
-    let wrapper_bg_y = parseInt(wrapper_bg_data.split(" ")[1]);
+    this.wrapper_bg_data = this.wrapper.style.backgroundPosition;
+    this.wrapper_bg_x = parseInt(this.wrapper_bg_data.split(" ")[0]);
+    this.wrapper_bg_y = parseInt(this.wrapper_bg_data.split(" ")[1]);
 
-    for (var i = 0; i < textNodes.length; i++) {
-      let node = textNodes[i];
+    for (var i = 0; i < this.textNodes.length; i++) {
+      let node = this.textNodes[i];
       node.style.color = 'transparent';
       node.style.position = 'absolute';
       node.style.backgroundImage = 'url(' + img_src_url + ')';
       node.style.webkitBackgroundClip = 'text';
       node.style.filter = 'invert('+this.invert+'%) grayscale('+this.grayscale+'%) contrast('+this.contrast+'%) saturate('+this.saturate+')';
 
-      let node_bg_x = node.offsetLeft, node_bg_y = node.offsetTop;
       let img = new Image();
       img.src = img_src_url;
 
-      if(wrapper_bg_data.length > 0) {
-        node_bg_x -= wrapper_bg_x || 0;
-        node_bg_y -= wrapper_bg_y || 0;
+      let node_bg_x = node.offsetLeft, node_bg_y = node.offsetTop;
+
+      if(this.wrapper_bg_data.length > 0) {
+        node_bg_x -= this.wrapper_bg_x || 0;
+        node_bg_y -= this.wrapper_bg_y || 0;
       }
 
       node.style.backgroundPosition = ((node_bg_x) * (-1)) + 'px ' + ((node_bg_y) * (-1)) + 'px';
 
     }
-    fn();
+
+    if(fn != undefined || fn != null)
+      fn(this);
+  }
+
+  update(eachCallback, callback) {
+
+    for (var i = 0; i < this.textNodes.length; i++) {
+      let node = this.textNodes[i];
+      let node_bg_x = node.offsetLeft, node_bg_y = node.offsetTop;
+      if(this.wrapper_bg_data.length > 0) {
+        node_bg_x -= this.wrapper_bg_x || 0;
+        node_bg_y -= this.wrapper_bg_y || 0;
+      }
+      node.style.backgroundPosition = ((node_bg_x) * (-1)) + 'px ' + ((node_bg_y) * (-1)) + 'px';
+
+      if(eachCallback != undefined || eachCallback != null)
+        eachCallback();
+    }
+
+    if(callback != undefined || callback != null)
+      callback();
+
   }
 
   getAllTextNodes(node){
